b00c20815f9494957384bf8c070601c35a9ce6b7
Matt Wormley <mwormley@adobe.com>
2018-05-06 09:25:53 -0700

    v1

--- background.js
+++ background.js
@@ -1,15 +1,55 @@
 var urlRegex = /https:\/\/www.blogger.com\/blogger.g.*editor\/target=post/;
 
-// A function to use as callback
-function printHrefs(hrefs) {
-}
+console.log("fetching...");
+fetch('https://raw.githubusercontent.com/matt-wormley/blogfix/master/picture_index.html')
+  .then(function (response) {
+    return response.text();
+  })
+  .then(function (picture_index_html) {
+    var parser = new DOMParser();
+    console.log("DONE");
+    var piDOM = parser.parseFromString(picture_index_html, "text/html").children[0];
+    var trElements = piDOM.getElementsByTagName('tr');
 
-// When the browser-action button is clicked...
-chrome.browserAction.onClicked.addListener(function (tab) {
-    // ...check the URL of the active tab against our pattern and...
-    if (urlRegex.test(tab.url)) {
-        // ...if it matches, send a message specifying a callback too
-        chrome.tabs.sendMessage(tab.id, {text: 'report_back'});
+    var pictureIndex = {};
+    var datePuller = /\((.*)\)/;
+    for (let i = 0; i < trElements.length; i++) {
+      const filename = trElements[i].children[0].textContent.trim().toLowerCase();
+      const hrefURL = new URL(trElements[i].children[1].children[0].href);
+      const title = trElements[i].children[1].children[0].textContent;
+      const date = title.match(datePuller)[1]
+
+      if (!pictureIndex.hasOwnProperty(filename)) {
+        pictureIndex[filename] = [];
+      }
+      pictureIndex[filename].push({
+        picturePath: hrefURL.pathname.substring(1),
+        date: date
+      });
     }
-});
 
+    console.log("DONE");
+
+    // When the browser-action button is clicked...
+    chrome.browserAction.onClicked.addListener(function (tab) {
+      // ...check the URL of the active tab against our pattern and...
+      if (urlRegex.test(tab.url)) {
+        // ...if it matches, send a message specifying a callback too
+        chrome.tabs.sendMessage(tab.id, {text: 'filterPictureIndex', pictureIndex: pictureIndex});
+      }
+    });
+
+    chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+      if (changeInfo.status == 'complete' && tab.active) {
+        function tryPictureIndex() {
+          chrome.tabs.sendMessage(tab.id, {text: 'filterPictureIndex', pictureIndex: pictureIndex}, (response) => {
+            if (response.retry) {
+              tryPictureIndex();
+            }
+          });
+        };
+
+        tryPictureIndex();
+      };
+    });
+  });
--- content.js
+++ content.js
@@ -1,16 +1,66 @@
 // Listen for messages
-chrome.runtime.onMessage.addListener(function (msg, sender) {
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     // If the received message has the expected format...
-    if (msg.text === 'report_back') {
-      var parser = new DOMParser();
-      var postHtml = document.getElementById("postingHtmlBox").value;
-      var post = parser.parseFromString(postHtml, "text/html").children[0];
+    if (msg.text === 'filterPictureIndex') {
+      console.log("trying");
+      try {
+        var pictureIndex = msg.pictureIndex;
+        var postHtml = document.getElementById('postingHtmlBox').value;
+        var parser = new DOMParser();
+        var post = parser.parseFromString(postHtml, 'text/html').children[0];
 
-      var anchorElements = Array.from(post.getElementsByTagName('a'));
-      var hrefs = anchorElements.map(a => a.href);
+        var anchorElements = Array.from(post.getElementsByTagName('a'));
+        var imgElements = Array.from(post.getElementsByTagName('img'));
 
-      for (i in hrefs) {
-        console.log('HREF: ' + hrefs[i]);
+        if (imgElements.length == 0) {
+          sendResponse({retry: true});
+          return;
+        }
+        var scrape = anchorElements.map((a,i) => {
+          return {
+            href: a.href,
+            alt: imgElements[i].alt.toLowerCase()
+          };
+        });
+
+        var fixdiv = document.createElement('div');
+        var insertInDiv = document.getElementsByClassName('optionHolder')[0]
+        insertInDiv.appendChild(fixdiv);
+
+        for (s in scrape) {
+          var filename = scrape[s].alt + '.jpg';
+          var pictureData = pictureIndex[filename];
+
+          var title = document.createElement('p');
+          title.textContent = '_______ ' +  filename + ' _______';
+          fixdiv.appendChild(title);
+
+          var dates = document.createElement('ul');
+          fixdiv.appendChild(dates);
+
+          for (p in pictureData) {
+            var date = document.createElement('li');
+            var a = document.createElement('a');
+            a.textContent = filename + " taken " + pictureData[p].date;
+            a.onmouseover = () => { document.getElementById('blogfixPreview').src ='file:///d:/' +  pictureData[p].picturePath; }
+            a.onmouseout = () => { document.getElementById('blogfixPreview').src = 'file:///d:/1x1.png'; }
+            date.appendChild(a);
+            dates.appendChild(date);
+          }
+        }
+
+        var imgParagraph = document.createElement('p');
+        var img = document.createElement('img');
+        img.id = "blogfixPreview";
+        fixdiv.appendChild(img);
+
+        console.log("success");
+        sendResponse({retry: false});
+      } catch(error) {
+        console.log("failure");
+        sendResponse({retry: false});
       }
+
+      return true;
     }
 });
